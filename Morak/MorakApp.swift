934e5c5e6d78ab8c56c45745ab265b5fdf249638
홍정민 <jeohong59@etomato.com>
2025-08-04 18:51:52 +0900

    Initial Commit

---
+++ Morak/MorakApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorakApp.swift
+//  Morak
+//
+//  Created by Hong jeongmin on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MorakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
