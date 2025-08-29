bbfc6758e21aacf7970e63971cc9cc7a49939468
jeohong <jeohong59@gmail.com>
2025-08-29 20:01:28 +0900

    [fix] 배경색 ZStack 에서 background 로 적용

--- Morak/Features/Post/Views/PostView.swift
+++ Morak/Features/Post/Views/PostView.swift
@@ -9,24 +9,20 @@ import SwiftUI
 
 struct PostView: View {
     var body: some View {
-        ZStack {
-            Color.primary
-                .ignoresSafeArea()
+        VStack(spacing: 0) {
+            HeaderView()
             
-            VStack(spacing: 0) {
-                HeaderView()
-                
-                GeometryReader { geometry in
-                    ScrollView {
-                        VStack {
-                            Text("포스트 뷰")
-                        }
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack {
+                        Text("포스트 뷰")
                     }
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
             }
         }
+        .background(Color.primary.ignoresSafeArea())
     }
 }
 
